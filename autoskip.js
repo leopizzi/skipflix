bef42a506994b62f85ccd74666f012558cab2a24
leopizzi <leopizzi94@gmail.com>
2019-04-05 21:04:14 -0300

    Added the timer function. Solving the pause after skip issue.

--- autoskip.js
+++ autoskip.js
@@ -6,13 +6,21 @@ const obs = new MutationObserver(mutations => mutations.map(matchAddedNodes));
 matchAddedNodes = mutation => mutation.addedNodes.forEach(matchAndClick);
 
 // Try matching a node for the "skip" button and click on positive match.
+
 matchAndClick = node => {
-  if (node.nodeType === 1 && node.matches('.skip-credits'))
-    click(node)
+	if (node.nodeType === 1 && node.matches('.skip-credits')) {
+		click(node);
+		setTimeout(function() {
+			if (document.querySelector('.button-nfplayerPlay')) {
+				document.querySelector('.button-nfplayerPlay').click();
+			}
+		}, 500)
+	}
 }
 
 // Click the node.
-click = node => node.firstChild.click();
+click = node => node.firstElementChild.click();
 
 // Observe the DOM for changes.
 obs.observe(document.documentElement, { childList: true, subtree: true });
+
